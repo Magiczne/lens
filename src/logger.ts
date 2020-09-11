e28ac42a6775f48907956789fc7509b27a794fa4
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-09-11 21:59:28 +0200

    Changed logger header padding

--- src/logger.ts
+++ src/logger.ts
@@ -10,7 +10,13 @@ class ConsoleLogger implements Logger {
 
     public header (message: string): void {
         console.log(chalk.cyan(boxen(message, {
-            borderStyle: boxen.BorderStyle.Round
+            borderStyle: boxen.BorderStyle.Round,
+            padding: {
+                top: 0,
+                right: 3,
+                bottom: 0,
+                left: 3
+            }
         })))
     }
 
