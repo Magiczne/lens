8b0f3fe108d20be251d3f654ff5b825eaf5c9524
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-09-22 16:15:05 +0200

    Added isStringArray and filterObject helper functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,5 @@
 type AsyncForEachCallback<T> = (value: T, index: number, array: T[]) => Promise<void>
 
-const forEachAsync = async <T>(array: T[], callback: AsyncForEachCallback<T>): Promise<void> => {
-    for (let i = 0; i < array.length; i++) {
-        await callback(array[i], i, array)
-    }
-}
-
 const arrayToChunks = <T>(array: T[], itemsPerChunk: number): Array<T[]> => {
     if (itemsPerChunk <= 0 || isNaN(itemsPerChunk)) {
         return [array]
@@ -24,7 +18,29 @@ const arrayToChunks = <T>(array: T[], itemsPerChunk: number): Array<T[]> => {
     }, [])
 }
 
+const filterObject = <T extends Record<string, unknown>>(obj: T, keys: Array<keyof T>): Partial<T> => {
+    return Object.keys(obj)
+        .filter(key => keys.includes(key))
+        .reduce((ret, key) => {
+            return Object.assign(ret, {
+                [key]: obj[key]
+            })
+        }, {})
+}
+
+const forEachAsync = async <T>(array: T[], callback: AsyncForEachCallback<T>): Promise<void> => {
+    for (let i = 0; i < array.length; i++) {
+        await callback(array[i], i, array)
+    }
+}
+
+const isStringArray = (array: Array<unknown>): array is Array<string> => {
+    return array.every(el => (typeof el === 'string'))
+}
+
 export {
     arrayToChunks,
-    forEachAsync
+    filterObject,
+    forEachAsync,
+    isStringArray
 }
